0a028a502ec03ddae80d014ecc0d047a22234ec9
Jesse <jessewilliamson@fastmail.fm>
2020-10-01 16:36:24 -0700

    move things around first pass looks good.

---
+++ index.js
@@ -0,0 +1,30 @@
+import text from './text';
+import build from 'markov-text-gen';
+
+const el = document.getElementById('message');
+const btn = document.getElementById('run');
+const charsOption = document.getElementById('chars');
+const loadingZone = document.getElementById('loading');
+const loadedZone = document.getElementById('loaded');
+
+const buildChains = (sourceText) => {
+  const buildByChars = build(sourceText, 10, true);
+  const buildByWords = build(sourceText, 3, false);
+  return Promise.all([buildByChars, buildByWords]);
+}
+
+const updateMessage= (runByChars, runByWords) => {
+  const byChars = charsOption.checked;
+  const generatedText = byChars ? runByChars(1000) : runByWords(100);
+  el.innerText = generatedText;
+  return;
+};
+
+buildChains(text)
+  .then(([runByChars, runByWords]) => {
+    updateMessage(runByChars, runByWords);
+    btn.addEventListener('click', updateMessage.bind(null, runByChars, runByWords));
+    loadingZone.classList.add('hide');
+    loadedZone.classList.remove('hide');
+});
+
